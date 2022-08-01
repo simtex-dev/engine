4c3e402388cb6312148fec8daa95ccc1001afb08
iaacornus <iaacornus.devel@gmail.com>
2022-08-01 22:33:27 +0800

    for stdout

---
+++ src/misc/signs.py
@@ -0,0 +1,6 @@
+class Signs:
+    PROC: str = "\033[1m[ PROC ]\033[0m"
+    FAIL: str = "\033[1m[ \033[31mFAIL\033[0m\033[1m ]\033[0m"
+    PASS: str = "\033[1m[ \033[32mPASS\033[0m\033[1m ]\033[0m"
+    INFO: str = "\033[1m[ INFO ]\033[0m"
+    INPT: str = "\033[1m[ INPT ]\033[0m"
