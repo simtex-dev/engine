71bb4dd0b3197194d56fbd0313d0b49399ab3dd8
iaacornus <iaacornus.devel@gmail.com>
2022-08-02 19:21:07 +0800

    fix type aliasing

--- src/misc/type_alias.py
+++ src/misc/type_alias.py
@@ -4,10 +4,10 @@ from typing import Any
 class DataTypes:
     """custom type annotations."""
 
-    TexPkg: list[str] # packages imported
-    SecSizes: dict[str, int] # sizes of sections e.g. subsection, etc ...
-    RawConf: dict[str, Any]
-    TexConf: tuple[ # general configuration file
+    TexPkg = list[str] # packages imported
+    SecSizes = dict[str, int] # sizes of sections e.g. subsection, etc ...
+    RawConf = dict[str, Any]
+    TexConf = tuple[ # general configuration file
             str,
             str,
             int,
