f5a9054a7bc26261b1625f39b82c8a215eb51478
iaacornus <iaacornus.devel@gmail.com>
2022-08-10 15:06:08 +0800

    write the contents into the file

--- src/utils/tex/sections/body.py
+++ src/utils/tex/sections/body.py
@@ -90,34 +90,36 @@ def body(
                     code: str; n: int
                     for n, code in enumerate(text_2[i+1:]):
                         if code.strip() == rules.code:
-                            out_file.write("\end{lstlisting}\n\n")
+                            out_file.write("\end{lstlisting}\n")
                             ref = n+i+1
                             break
+
+                        out_file.write(code)
+                else:
+                    parts: list[str] = line.split()
+
+                    part: str
+                    for part in parts:
+                        img_results: list[tuple[str, str]]
+                        link_results: list[tuple[str, str]]
+
+                        if (img_results := findall(rules.image, part)):
+                            out_file.write(
+                                (
+                                    "\n\\begin{figure}\n"
+                                    "\t\\includegraphics[width=\\textwidth]"
+                                    f"{{{img_results[0][1]}}}\n"
+                                    f"\t\\caption{img_results[0][0]}\n"
+                                    "\\end{figure}\n"
+                                )
+                            )
+                        elif (link_results := findall(rules.links, part)):
+                            links: tuple[str, str] = link_results[0]
+                            new_line: str = line.replace(
+                                    f"[{links[0]}]({links[1]})",
+                                    f"\\href{{{links[0]}}}{{{links[1]}}}"
+                                )
+                            out_file.write(f"\n{new_line}\n")
                         else:
-                            out_file.write(f"{code}")
-
-                parts: list[str] = line.split()
-
-                part: str
-                for part in parts:
-                    img_results: list[tuple[str, str]]
-                    link_results: list[tuple[str, str]]
-
-                    if (img_results := findall(rules.image, part)):
-                        out_file.write(
-                            (
-                                "\n\\begin{figure}\n"
-                                "\t\\includegraphics[width=\\textwidth]"
-                                f"{{{img_results[0][1]}}}\n"
-                                f"\t\\caption{img_results[0][0]}\n"
-                                "\\end{figure}\n"
-                            )
-                        )
-                    elif (link_results := findall(rules.links, part)):
-                        links: tuple[str, str] = link_results[0]
-                        new_line: str = line.replace(
-                                f"[{links[0]}]({links[1]})",
-                                f"\\href{{{links[0]}}}{{{links[1]}}}"
-                            )
-                        out_file.write(f"\n{new_line}\n")
-
+                            out_file.write(f"\n{line}")
+                            break
