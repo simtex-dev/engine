191ea58da2e01367d68c5eebe48787eda6486f3e
iaacornus <iaacornus.devel@gmail.com>
2022-08-12 20:11:39 +0800

    initial commit on main program

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from src.utils.config_fetch import ConfParse
+from src.utils.logger import Logger
+from src.utils.tex.sections.headings import headings
+from src.utils.tex.sections.body import body, format_body
+
+
+def main() -> None:
+    ...
