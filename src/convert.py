2f1f0523dfc619aa925df04179c191e6058c1c77
iaacornus <iaacornus.devel@gmail.com>
2022-08-14 23:53:06 +0800

    use more consistent parameter name

--- src/convert.py
+++ src/convert.py
@@ -4,7 +4,6 @@ from os import mkdir
 from .misc.stdout import Signs
 
 from src.utils.config import Config, Rules
-from src.utils.config import Config
 from src.utils.logger import Logger
 from src.utils.tex.sections.headings import headings
 from src.utils.tex.sections.body import body, format_body
@@ -13,17 +12,17 @@ from src.utils.tex.sections.body import body, format_body
 def convert(
         log: Logger,
         rules: Rules,
-        conf: Config,
+        config: Config,
         title: str,
         in_file: str,
     ) -> None:
     """Main program."""
 
-    if not exists(conf.output_folder):
-        mkdir(conf.output_folder)
+    if not exists(config.output_folder):
+        mkdir(config.output_folder)
 
     OFILE_PATH: str
-    if exists((OFILE_PATH := f"{conf.output_folder}/{conf.filename}")):
+    if exists((OFILE_PATH := f"{config.output_folder}/{config.filename}")):
         if input(
                 f"{Signs.INPT} File: {OFILE_PATH} already exists, overwrite?"
             ).lower() == "n":
@@ -32,7 +31,7 @@ def convert(
 
     out_file: TextIO
     with open(OFILE_PATH, "w", encoding="utf-8") as out_file:
-        start: int = headings(log, conf, title, out_file)
+        start: int = headings(log, config, title, out_file)
         body(log, rules, in_file, out_file)
 
-    format_body(log, start, OFILE_PATH)
+    format_body(log, config, start, OFILE_PATH)
