aedf6bd83edc291e632328229ce524d80dc36980
iaacornus <iaacornus.devel@gmail.com>
2022-08-14 23:38:38 +0800

    include prompt before overwriting the currently existing file

--- src/convert.py
+++ src/convert.py
@@ -1,6 +1,7 @@
 from typing import TextIO
 from os.path import exists
 from os import mkdir
+from .misc.stdout import Signs
 
 from src.utils.config import Config, Rules
 from src.utils.config import Config
@@ -21,7 +22,14 @@ def convert(
     if not exists(conf.output_folder):
         mkdir(conf.output_folder)
 
-    OFILE_PATH: str = f"{conf.output_folder}/{conf.filename}"
+    OFILE_PATH: str
+    if exists((OFILE_PATH := f"{conf.output_folder}/{conf.filename}")):
+        if input(
+                f"{Signs.INPT} File: {OFILE_PATH} already exists, overwrite?"
+            ).lower() == "n":
+            log.logger("E", f"File: {OFILE_PATH} already exists.")
+            raise SystemExit
+
     out_file: TextIO
     with open(OFILE_PATH, "w", encoding="utf-8") as out_file:
         start: int = headings(log, conf, title, out_file)
