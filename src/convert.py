7d0292608c0708be285fedbd7d33bd1d9b71b00c
iaacornus <iaacornus.devel@gmail.com>
2022-08-16 22:08:22 +0800

    use filename as title if title is none

--- src/convert.py
+++ src/convert.py
@@ -13,13 +13,19 @@ def convert(
         log: Logger,
         rules: Rules,
         config: Config,
-        title: str,
+        _title: str,
         in_file: str,
     ) -> None:
     """Main program."""
 
     if not exists(config.output_folder):
         mkdir(config.output_folder)
+
+    if _title is None:
+        _title = "".join(in_file.split("/")[-1])
+        print(
+            f"{Signs.INFO} Title is none, using the {_title} as title."
+        )
 
     OFILE_PATH: str
     if exists((OFILE_PATH := f"{config.output_folder}/{config.filename}")):
@@ -31,7 +37,7 @@ def convert(
 
     out_file: TextIO
     with open(OFILE_PATH, "w", encoding="utf-8") as out_file:
-        start: int = headings(log, config, title, out_file)
+        start: int = headings(log, config, _title, out_file)
         body(log, rules, in_file, out_file)
 
     format_body(log, config, start, OFILE_PATH)
