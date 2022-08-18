e044406f0dc091c2efee492e8011b057c18fad17
iaacornus <iaacornus.devel@gmail.com>
2022-08-19 00:14:18 +0800

    update unit test case for rules

--- tests/tests.py
+++ tests/tests.py
@@ -84,7 +84,11 @@ class TestCases(unittest.TestCase):
                 subparagraph="#####",
                 inline_math="$",
                 paragraph_math="$$",
-                inline_code="`(.*?)`"
+                inline_code="`(.*?)`",
+                bold=["**", "\\*\\*(.*?)\\*\\*"],
+                italics=["_", "_(.*?)_"],
+                emph=["!*", "!\\*(.*?)!\\*"]
             ),
             rules
         )
+
