2098f76a2e423629b6675c6d2b5c2a87d36ebc70
iaacornus <iaacornus.devel@gmail.com>
2022-08-18 12:40:24 +0800

    move other functions to decrease the lenght of the body()

--- src/utils/tex/sections/body.py
+++ src/utils/tex/sections/body.py
@@ -1,9 +1,11 @@
 from re import findall
 from turtle import title
 
-from typing import Any, Callable, Optional, TextIO, IO
+from typing import Any, Callable, TextIO, IO
 
 from src.config import Config, Rules
+from src.utils.tex.environments.mathsec import mathsec
+from src.utils.tex.environments.commons import commons
 from src.utils.logger import Logger
 
 
@@ -18,7 +20,7 @@ def body(
     out_file: textio -- where the output will be written.
     """
 
-    line: str; def_rule: str
+    line: str
     striptitle: Callable[..., str] = lambda line, def_rule: (
             line
                 .replace(def_rule, "")
@@ -39,7 +41,7 @@ def body(
 
     log.logger("I", "Writing the body to the document ...")
 
-    i: int; line: str
+    i: int
     for i, line in enumerate(ref_tex):
         if line in ["", "\n"]:
             continue
@@ -70,36 +72,14 @@ def body(
                 )
             case _:
                 if line.startswith(rules.paragraph_math): # math mode
-                    maths: list[str] = []
-
-                    if line.strip() == rules.paragraph_math: # for align
-                        out_file.write("\\begin{align}\n")
-
-                        eqs: str; j: int
-                        for j, eqs in enumerate(ref_tex.copy()[i+1:]):
-                            if eqs.strip() == rules.paragraph_math:
-                                ref = j+i+1
-                                break
-
-                            if "&" not in eqs:
-                                eqs = eqs.replace("=", "&=", 1)
-
-                            eqs = eqs.replace("\n", "")
-                            maths.append(f"{eqs}\\\\\n")
-
-                        maths[-1] = maths[-1].replace("\\\\\n", "\n")
-                        for eqs in maths:
-                            out_file.write(f"\t{eqs}")
-
-                        out_file.write("\\end{align}\n")
-                    else: # for single line/equation
-                        out_file.write(
-                            (
-                                "\\begin{equation}\n"
-                                f"\t{line[2:-3]}\n"
-                                "\\end{equation}\n"
-                            )
-                        )
+                    ref = mathsec(
+                        line,
+                        rules.paragraph_math,
+                        out_file,
+                        ref_tex.copy(),
+                        i,
+                        ref
+                    )
                 elif line.startswith(rules.code): # for code blocks
                     language: str = line[3:].replace("\n", "")
                     out_file.write(
@@ -116,100 +96,13 @@ def body(
 
                         out_file.write(code)
                 else:
-                    parts: list[str] = line.split(" ")
-                    skip_line: bool = False
+                    commons(
+                        line,
+                        line.split(" "),
+                        rules,
+                        files,
+                        out_file
+                    )
 
-                    part: str
-                    try:
-                        new_line: str = line
-
-                        for part in parts:
-                            img_results: list[tuple[str, str]]
-                            link_results: list[tuple[str, str]]
-
-                            if (img_results := findall(rules.image, part)):
-                                out_file.write(
-                                    "\\begin{figure}[h]\n"
-                                    "\t\\includegraphics[width=\\textwidth]"
-                                    f"{{{img_results[0][1]}}}\n"
-                                    f"\t\\caption{{{img_results[0][0]}}}\n"
-                                    "\\end{figure}\n"
-                                )
-                                skip_line = True
-                                files.append(img_results[0][1])
-                                break
-
-                            if (link_results := findall(rules.links, part)):
-                                link: tuple[str, str]
-                                for link in link_results:
-                                    new_line = new_line.replace(
-                                            f"[{link[0]}]({link[1]})",
-                                            f"\\href{{{link[0]}}}{{{link[1]}}}"
-                                        )
-                            elif (
-                                    inline_codes := findall(
-                                            rules.inline_code, part
-                                        )
-                                ):
-                                codes: tuple[str]
-                                for codes in inline_codes:
-                                    new_line = new_line.replace(
-                                            f"`{codes}`",
-                                            f"\\texttt{{{codes}}}"
-                                        )
-                    finally:
-                        if not skip_line:
-                            new_line = (
-                                    new_line
-                                        .replace("_", r"\_")
-                                        .replace("\n", "\n")
-                                )
-                            out_file.write(f"\n{new_line}\n")
 
     return files
-
-
-def format_body(
-        log: Logger, config: Config, start: int, out_file: str
-    ) -> None:
-    """Format the document body of the generated LaTeX file."""
-
-    try:
-        ref: TextIO
-        with open(out_file, "r", encoding="utf-8") as ref:
-            ref_tex: list[str] = ref.readlines()
-
-        log.logger("I", "Formatting the document ...")
-
-        ignore: int = -1
-
-        file: IO[Any]
-        with open(out_file, "w", encoding="utf-8") as file:
-            line: str
-            for line in ref_tex[:start]:
-                file.write(line)
-
-            file.write("\n\\begin{document}\n")
-
-            if config.make_title:
-                file.write("\t\maketitle\n")
-
-            i: int
-            for i, line in enumerate(ref_tex[start:]):
-                if i < ignore:
-                    continue
-
-                if line.startswith(r"\begin{lstlisting}"):
-                    for j, codes in enumerate(ref_tex.copy()[i+start:]):
-                        file.write(codes)
-                        if codes.startswith(r"\end{lstlisting}"):
-                            ignore = j+i+1
-                            break
-
-                    continue
-                file.write(f"\t{line}")
-            file.write("\\end{document}")
-    except (FileNotFoundError, OSError, PermissionError, IOError) as Err:
-        log.logger(
-            "E", f"{Err}. Cannot format the document, aborting ..."
-        )
