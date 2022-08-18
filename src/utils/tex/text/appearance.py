385205f65e035b01464363aa622c1b15e3525281
iaacornus <iaacornus.devel@gmail.com>
2022-08-18 22:31:11 +0800

    for handling of text appearances: bold, emph, italics

--- src/utils/tex/text/appearance.py
+++ src/utils/tex/text/appearance.py
@@ -3,14 +3,21 @@ from re import findall
 from src.config import Rules
 
 
-def appearance(line: str, parts: list[str], rules: Rules) -> str:
-    part: str
-    for part in parts:
-        if (bold := findall(rules.bold, part)):
-            line = line.replace(part, f"\\textbf{{{bold[0]}}}")
-        elif (italics := findall(rules.italics, part)):
-            line = line.replace(part, f"\\textit{{{italics[0]}}}")
-        elif (emph := findall(rules.emph, part)):
-            line = line.replace(part, f"\\emph{{{emph[0]}}}")
-
+def appearance(line: str, size: int, rules: Rules) -> str:
+    for _ in range(size):
+        if (bold := findall(rules.bold[1], line)):
+            line = line.replace(
+                    f"{rules.bold[0]}{bold[0]}{rules.bold[0]}",
+                    f"\\textbf{{{bold[0]}}}"
+                )
+        elif (italics := findall(rules.italics[1], line)):
+            line = line.replace(
+                    f"{rules.italics[0]}{italics[0]}{rules.italics[0]}",
+                    f"\\textit{{{italics[0]}}}"
+                )
+        elif (emph := findall(rules.emph[1], line)):
+            line = line.replace(
+                    f"{rules.emph[0]}{emph[0]}{rules.emph[0]}",
+                    f"\\emph{{{emph[0]}}}"
+                )
     return line
