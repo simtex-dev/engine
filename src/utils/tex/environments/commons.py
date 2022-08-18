ffe8673a9003b5e57e37177baf518ff23ff1f1a2
iaacornus <iaacornus.devel@gmail.com>
2022-08-19 00:35:07 +0800

    refactor the codebase
    changes include
    - remove some of the actions under iteration
    - remove unused and unnecessary variables
    - fix bugs in the wrongfully passed argument in appearance()
    - simplify the syntax

--- src/utils/tex/environments/commons.py
+++ src/utils/tex/environments/commons.py
@@ -7,55 +7,46 @@ from src.utils.tex.text.appearance import appearance
 
 def commons(
         line: str,
-        parts: list[str],
+        linelen: int,
         rules: Rules,
         files: list[str],
         out_file: TextIO
     ) -> None:
     try:
-        new_line: str = line
         skip_line: bool = False
 
-        part: str
-        for part in parts:
-            img_results: list[tuple[str, str]]
-            link_results: list[tuple[str, str]]
+        img: list[tuple[str, str]]
+        link: list[tuple[str, str]]
 
-            if (img_results := findall(rules.image, part)):
-                out_file.write(
-                    "\\begin{figure}[h]\n"
-                    "\t\\includegraphics[width=\\textwidth]"
-                    f"{{{img_results[0][1]}}}\n"
-                    f"\t\\caption{{{img_results[0][0]}}}\n"
-                    "\\end{figure}\n"
-                )
-                skip_line = True
-                files.append(img_results[0][1])
-                break
+        if (img := findall(rules.image, line)):
+            out_file.write(
+                "\\begin{figure}[h]\n"
+                "\t\\includegraphics[width=\\textwidth]"
+                f"{{{img[0][1]}}}\n"
+                f"\t\\caption{{{img[0][0]}}}\n"
+                "\\end{figure}\n"
+            )
+            skip_line = True
+            files.append(img[0][1])
 
-            if (link_results := findall(rules.links, part)):
-                link: tuple[str, str]
-                for link in link_results:
-                    new_line = new_line.replace(
-                            f"[{link[0]}]({link[1]})",
-                            f"\\href{{{link[0]}}}{{{link[1]}}}"
-                        )
-            elif (
-                    inline_codes := findall(
-                            rules.inline_code, part
-                        )
-                ):
-                codes: tuple[str]
-                for codes in inline_codes:
-                    new_line = new_line.replace(
-                            f"`{codes}`",
-                            f"\\texttt{{{codes}}}"
-                        )
+        for _ in range(linelen):
+            print(line)
+            if (link := findall(rules.links, line)):
+                line = line.replace(
+                        f"[{link[0][0]}]({link[0][1]})",
+                        f"\\href{{{link[0][0]}}}{{{link[0][1]}}}"
+                    )
+            elif (icodes := findall(rules.inline_code, line)):
+                print(icodes)
+                line = line.replace(
+                        f"`{icodes[0]}`",
+                        f"\\texttt{{{icodes[0]}}}"
+                    )
     finally:
         if not skip_line:
-            new_line = (
-                    appearance(line, len(parts), rules)
+            line = (
+                    appearance(line, linelen, rules)
                         .replace("_", r"\_")
                         .replace("\n", "\n")
                 )
-            out_file.write(f"\n{new_line}\n")
+            out_file.write(f"\n{line}\n")
