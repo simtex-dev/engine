d81cbb8f6b961743076a202606d719c8879e7ff0
iaacornus <iaacornus.devel@gmail.com>
2022-08-21 00:34:20 +0800

    include the new parameters

--- src/utils/config_fetch.py
+++ src/utils/config_fetch.py
@@ -112,5 +112,7 @@ class ConfParse:
             raw_conf["DATE"],
             raw_conf["MAKE_TITLE"],
             raw_conf["FILE_NAME"],
-            raw_conf["OUTPUT_FOLDER"]
+            raw_conf["OUTPUT_FOLDER"],
+            raw_conf["COMPILER"],
+            raw_conf["ENCODE"]
         )
