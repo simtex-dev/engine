fc10527a900c3dbf5133e0d0c44197e2c39fa10c
iaacornus <iaacornus.devel@gmail.com>
2022-08-21 20:08:56 +0800

    fix compiler override

--- src/mutils/update_conf.py
+++ src/mutils/update_conf.py
@@ -15,6 +15,28 @@ def update_conf(log: Logger, config: Config, args: Any) -> None:
         args -- overrides received from arguments.
     """
 
+    if args.compiler is not None and args.compiler not in (
+            compilers := ["xetex", "pdflatex", "luatex"]
+        ): # if compiler is not in options
+        pos_compilers: list[float] = [
+                SeqMatch(
+                    args.compiler, option
+                ).ratio() for option in compilers
+            ]
+        compiler: str = compilers[
+                pos_compilers.index(max(pos_compilers))
+            ]
+
+        if input(
+                f"\033[1mINPT\033[0m Did you mean {compiler}? "
+            ).lower() == "y":
+            args.compiler = compiler
+        else:
+            args.compiler = "pdflatex"
+            log.logger(
+                "e", "Compiler option not recognized, using pdflatex"
+            )
+
     PARAMETERS: dict[str, Any] = {
         "filename": args.filename,
         "output_folder": args.outputfolder,
@@ -33,28 +55,6 @@ def update_conf(log: Logger, config: Config, args: Any) -> None:
     key_: str; param: Any
     for key_, param in PARAMETERS.items(): # for overrides
         if param is not None:
-            if args.compiler is not None and args.compiler not in (
-                    compilers := ["xetex", "pdflatex", "luatex"]
-                ): # if compiler is not in options
-                pos_compilers: list[float] = [
-                        SeqMatch(
-                            args.compiler, option
-                        ).ratio() for option in compilers
-                    ]
-                compiler: str = compilers[
-                        pos_compilers.index(max(pos_compilers))
-                    ]
-
-                if input(
-                        f"\033[1mINPT\033[0m Did you mean {compiler}? "
-                    ).lower() == "y":
-                    args.compiler = compiler
-                else:
-                    args.compiler = "pdflatex"
-                    log.logger(
-                        "e", "Compiler option not recognized, using pdflatex"
-                    )
-
             log.logger(
                 "I",
                 f"Override: {key_} {config.__getattribute__(key_)} -> {param}"
