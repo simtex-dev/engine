cb90c1c879feeac0d166394f05075745f4111ff8
iaacornus <iaacornus.devel@gmail.com>
2022-08-25 01:27:03 +0800

    include param to fetch config from devel or main

--- src/mutils/fix_missing_conf.py
+++ src/mutils/fix_missing_conf.py
@@ -7,7 +7,8 @@ def fix_missing_config(
         log: Logger,
         CONF_PATH: str,
         conf: bool = False,
-        code_conf: bool = False
+        code_conf: bool = False,
+        devel: bool = False
     ) -> None:
     """Downloads the original config file from github if not found.
 
@@ -16,23 +17,33 @@ def fix_missing_config(
         CONF_PATH -- path of the config file.
         conf, code_conf -- whether the missing config is the code config
             or the main, simtex.json.
+        devel -- whether to pull from devel or stable branch.
     """
+
+    # this was done instead of packaging the config from
+    # package data, so that the user will be able to
+    # interact with the config file easily.
+
+    if devel:
+        branch: str = "devel"
+    else:
+        branch = "main"
 
     if conf:
         link: str = (
                 "https://raw.githubusercontent.com/iaacornus"
-                "/simtex/main/examples/config/simtex.json"
+                f"/simtex/{branch}/examples/config/simtex.json"
             )
         filename: str = "simtex.json"
     elif code_conf:
         link = (
                 "https://raw.githubusercontent.com/iaacornus"
-                "/simtex/main/examples/config/code_conf.txt"
+                f"/simtex/{branch}/examples/config/code_conf.txt"
             )
         filename = "code_conf.txt"
 
     log.logger(
-        "e", f"Config file: {filename} not found, fetching the default."
+        "e", f"Config file: {filename} not found, fetching the default ..."
     )
 
     for _ in range(3):
