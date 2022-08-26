22e2a137e1fb903a9a120bea14d21fa8a1c7e2fb
iaacornus <iaacornus.devel@gmail.com>
2022-08-26 20:41:54 +0800

    for merging of two different code

--- src/mutils/merge_conf.py
+++ src/mutils/merge_conf.py
@@ -1,27 +1,50 @@
-from json import load
+from json import load, dump
 from typing import Any, TextIO
 
 from src.utils.logger import Logger
 
 
-def merge_conf(log: Logger, conf_path: str) -> None:
+def merge_conf(log: Logger, CONF_PATH: str) -> None:
     """When fixing the two config files due to missing parameter, the
     other parameters should not be overwritten by the default config
     that will be fetched from the repository, thus the two should be
-    merged instead."""
+    merged instead.
+
+    Args:
+        log -- for logging.
+        CONF_PATH -- path of the config file.
+    """
 
     try:
+        # in this context, Any type hint implies the value is either
+        #   int, list[Any], float, dict[str, Any].
         temp_conf: dict[str, Any] = {}
 
-        conf_ref: TextIO; conf: TextIO;
+        new_conf: TextIO; conf_ref: TextIO;
         with open(
-                f"{conf_path}/simtex.json.bak", "r", encoding="utf-8"
-            ) as conf_ref, open(
-                f"{conf_path}/simtex.json", "r", encoding="utf-8"
-            ) as conf:
-            conf: dict[str, Any] = load(conf)
-            conf_ref: dict[str, Any] = load(conf_ref)
+                f"{CONF_PATH}/simtex.json.bak", "r", encoding="utf-8"
+            ) as new_conf, open(
+                f"{CONF_PATH}/simtex.json", "r", encoding="utf-8"
+            ) as conf_ref:
+            conf_ref_: dict[str, Any] = load(conf_ref)
+            new_conf_: dict[str, Any] = load(new_conf)
 
+        param: str; new_val: Any; conf_val: Any
+        for old_conf, new_conf in zip(
+                [ref for ref in conf_ref_], [new for new in new_conf_]
+            ):
+            for param, new_val in new_conf.items():
+                if (conf_val := old_conf.get(param, new_val)) != new_val:
+                    temp_conf[param] = conf_val
+                else:
+                    temp_conf[param] = new_val
+
+        print(temp_conf)
+        fixed_conf: TextIO
+        with open(
+                f"{CONF_PATH}/simtex.json", "w", encoding="utf-8"
+            ) as fixed_conf:
+            dump(temp_conf, fixed_conf, indent=4)
     except FileNotFoundError as Err:
         log.logger(
             "e", f"{Err}. Cannot merge the two config files, skipping ..."
