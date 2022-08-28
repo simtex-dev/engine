8431e2e51effb17eb96a63803aac4342c59d5c19
iaacornus <iaacornus.devel@gmail.com>
2022-08-28 16:51:58 +0800

    initial commit for enumerate/list environment

---
+++ src/utils/tex/environments/enumerate.py
@@ -0,0 +1,13 @@
+from typing import TextIO
+
+
+def enumerate(start: int, out_file: TextIO) -> int:
+    """For enumerate/lists environment.
+
+    Args:
+        out_file -- where the output will be written.
+
+    Returns:
+        The index of line where the enumerate ends.
+    """
+    ...
