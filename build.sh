cbf8ad6da61f644b3bbc535c1b14c79c944bc63c
iaacornus <iaacornus.devel@gmail.com>
2022-09-04 00:01:03 +0800

    include more commands to execute

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 if [[ ! $(command -v python &> /dev/null) ]]; then 
     python="python3.10"
 else
@@ -8,6 +10,8 @@ if [[ ! $(command -v pyinstaller &> /dev/null) ]]; then
     python -m pip install pyinstaller
 fi
 
+pip install -r requirements.txt
+pip install setuptools build
 pyinstaller src/main.py --name simtex --onefile -y
-
-
+python setup.py sdist
+python -m build
