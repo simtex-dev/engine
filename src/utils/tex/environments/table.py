c4b164396beab66f83f83aa93f192fbb4193ef7a
iaacornus <iaacornus.devel@gmail.com>
2022-09-06 19:50:10 +0800

    for writing of parsed table in the out file

---
+++ src/utils/tex/environments/table.py
@@ -0,0 +1,46 @@
+from typing import TextIO
+
+from src.config import Rules, Replacements
+from src.utils.tex.text.format import format
+from src.utils.tex.parser.table_parse import table_parse
+
+def table(
+        rules: Rules,
+        replacements: Replacements,
+        start: int,
+        source: list[str],
+        out_file: TextIO
+    ) -> int:
+    """Write the parsed table to the body of the LaTeX file.
+
+    Args:
+        rules: Rules -- rules that needs to be followed in translation.
+        replacements -- math symbols that will be replaced with latex commands.
+        start -- where the parser/translator would start.
+        source -- where the other rows would be found.
+        out_file -- where the translated line will be written.
+
+    Returns:
+        An integer that denotes what line to skip.
+    """
+
+    out_file.write("\\begin{center}\n")
+
+    cur: int; row: str
+    for cur, row in enumerate(source[start:]):
+        if "|" not in row:
+            end: int = cur+start
+
+        formatted_row: str = format(
+                rules, replacements, row, row.split("|")
+            )
+        parsed: str | tuple[int, list[str]] = table_parse(cur, formatted_row)
+
+        if isinstance(parsed, tuple):
+            cols: str = " | ".join(["c" for _ in parsed[1]])
+            out_file.write(f"\n\t\\begin{{tabular}}{{{cols}}}\n")
+        elif isinstance(parsed, str):
+            out_file.write(f"\t\t{parsed}\n")
+
+    out_file.write("\t\\end{tabular}\n\\end{center}\n")
+    return end
