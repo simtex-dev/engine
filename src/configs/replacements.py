783e940c334562fcb61c3bfd43fb922ab6d6143f
iaacornus <iaacornus.devel@gmail.com>
2022-09-08 18:30:57 +0800

    update docstring of the config file

--- src/configs/replacements.py
+++ src/configs/replacements.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass
 
 @dataclass(frozen=True)
 class Replacements:
-    """Replacement of some math symbols and their respective LaTeX commands."""
+    """Replacement of some math symbols, that may either be UTF8
+    or raw utf8 symbols with their respective LaTeX commands."""
 
     replacements: dict[str, str]
--- src/configs/rules.py
+++ src/configs/rules.py
@@ -3,7 +3,38 @@ from dataclasses import dataclass
 
 @dataclass(frozen=True)
 class Rules:
-    """Dataclass for configuration of the parser."""
+    """Dataclass for configuration of the parser, this includes
+    the markers that the parser will look for as well as the patterns.
+
+    Params:
+        files -- the type of files that simtex will convert.
+        code -- the marker of code blocks lstlistings.
+        image -- marker of figure/image insert \includegraphic.
+        links -- hyperlinks in line \hyperlink.
+        section -- \section
+        sectionn -- \section*
+        subsection -- \subsection
+        subsectionn -- \subsection*
+        subsubsection -- \subsubsection
+        subsubsectionn -- \subsubsectionn
+        paragraph -- \paragraph
+        paragraphn -- \paragraphn
+        subparagraph -- \subparagraph
+        subparagraphn -- \subparagraph*
+        paragraph_math -- for equation/align environment.
+        inline_math -- for inline math $$.
+        inline_code -- for inline code -> \\texttt.
+        bold -- \\textbf
+        italics -- \\textit
+        emph -- \emph
+        strike -- \sout
+        supscript -- \\textsuperscript
+        subscript -- \\textsubscript
+        uline -- \\underline
+        quote -- ``<text>''
+        bquote -- block quote using csquote.
+        nonum -- for numberless paragraph math lines.
+    """
 
     files: list[str]
     code: str
