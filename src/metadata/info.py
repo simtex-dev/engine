0ead909c96a3f91a062e71a89148e3ee60af4daf
iaacornus <iaacornus.devel@gmail.com>
2022-09-12 06:47:42 +0800

    include description

--- src/metadata/info.py
+++ src/metadata/info.py
@@ -4,3 +4,8 @@ class PkgInfo:
     __version__: str = "0.4-beta"
     __author__: str = "James Aaron Erang (iaacornus)"
     __author_email__: str = "iaacornus.devel@gmail.com"
+    __description__: str = (
+            "Convert your markdown or text files into LaTeX/pdf"
+            " with one command!\n Interop with LaTeX and markdown"
+            " is also allowed by simtex!"
+        )
