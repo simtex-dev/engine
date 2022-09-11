1513edb2ba34cedc04eefacc33e004262f940644
iaacornus <iaacornus.devel@gmail.com>
2022-09-11 22:03:15 +0800

    move the function calls from src.cli

--- src/utils/convert.py
+++ src/utils/convert.py
@@ -1,9 +1,11 @@
 from os.path import isdir
+from subprocess import Popen
 from typing import Any, NoReturn
 
 from src.configs.config import Config
 from src.configs.rules import Rules
 from src.configs.replacements import Replacements
+from src.mutils.build_tex import build_file
 from src.utils.convert_file import convert_file
 from src.mutils.find_files import find_files
 from src.utils.logger import Logger
@@ -16,7 +18,7 @@ def convert(
         config: Config,
         replacement: Replacements,
         autocorrect: bool = False
-    ) -> str | NoReturn:
+    ) -> list[str] | NoReturn:
     """Call the converter to convert the files.
 
     Args:
@@ -30,9 +32,10 @@ def convert(
         autocorrect -- whether to toggle autocorrect.
 
     Returns:
-        The filepath of the output file.
+        The path(s) of the converted file.
     """
 
+    file_path: list[str] = []
 
     if isdir(args.input):
         log.logger(
@@ -50,23 +53,51 @@ def convert(
             log.logger(
                 "I", f"Converting the {cur} in directory: {args.input}"
             )
+            file_path.append(
+                convert_file(
+                    log,
+                    args,
+                    rules,
+                    config,
+                    replacement,
+                    file,
+                    autocorrect
+                )
+            )
+    else:
+        file_path.append(
             convert_file(
                 log,
                 args,
                 rules,
                 config,
                 replacement,
-                file,
+                args.input,
                 autocorrect
             )
-    else:
-        convert_file(
-            log,
-            args,
-            rules,
-            config,
-            replacement,
-            args.input,
-            autocorrect
         )
 
+    if args.build:
+        for file in file_path:
+            build_file(
+                log,
+                config.compiler,
+                config.output_folder,
+                file,
+                args.verbose
+            )
+            if args.buildnview:
+                try:
+                    Popen(["xgd-open", file])
+                except FileNotFoundError:
+                    log.logger(
+                        "e", "No PDF viewer found, cannot view PDF file."
+                    )
+    else:
+        print(
+            "\033[34mINFO \033[0m\t To compile the output, you "
+            "use can overleaf: \033[36mhttps://www.overleaf.com/"
+            "\033[0m (not sponsored) to compile the output."
+        )
+
+    return file_path
