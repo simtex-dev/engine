bbb20cd746323c55e984c8f4c6024c9fc85181e2
iaacornus <iaacornus.devel@gmail.com>
2022-09-12 06:42:50 +0800

    metadata container of the package

---
+++ src/metadata/info.py
@@ -0,0 +1,6 @@
+class PkgInfo:
+    """Some metadata of the package."""
+
+    __version__: str = "0.4-beta"
+    __author__: str = "James Aaron Erang (iaacornus)"
+    __author_email__: str = "iaacornus.devel@gmail.com"
