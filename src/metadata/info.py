9046afbb34648d9c08004757beb619d49597dbd2
iaacornus <iaacornus.devel@gmail.com>
2022-09-15 23:56:29 +0800

    bump pkg ver to patch 1

--- src/metadata/info.py
+++ src/metadata/info.py
@@ -1,7 +1,7 @@
 class PkgInfo:
     """Some metadata of the package."""
 
-    __version__: str = "0.4-beta"
+    __version__: str = "0.4.1-beta"
     __author__: str = "James Aaron Erang (iaacornus)"
     __author_email__: str = "iaacornus.devel@gmail.com"
     __description__: str = (
