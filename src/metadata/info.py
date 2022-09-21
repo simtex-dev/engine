23d858fbe5011d163e32e4ede697a813949cac58
iaacornus <iaacornus.devel@gmail.com>
2022-09-21 18:40:44 +0800

    update info to 0.4.2b1 for pypi re release

--- src/metadata/info.py
+++ src/metadata/info.py
@@ -1,7 +1,7 @@
 class PkgInfo:
     """Some metadata of the package."""
 
-    __version__: str = "0.4.2-beta"
+    __version__: str = "0.4.2-beta-1"
     __author__: str = "James Aaron Erang (iaacornus)"
     __author_email__: str = "iaacornus.devel@gmail.com"
     __description__: str = (
